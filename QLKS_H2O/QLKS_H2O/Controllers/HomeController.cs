f6d8696673256c162a75dc7f12636143c910c89b
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-07-29 16:21:14 +0700

    complete project Hotel H2o

---
+++ QLKS_H2O/QLKS_H2O/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using QLKS_H2O.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLKS_H2O.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLKS_H2OEntities db = new QLKS_H2OEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var loaiPhongs = db.LOAIPHONGs.ToList().Select(lp =>
+            {
+                LoaiPhongGioiThieu loaiPhongGioiThieu = new LoaiPhongGioiThieu();
+                loaiPhongGioiThieu.tenLP = lp.TEN_LOAIPHONG;
+                loaiPhongGioiThieu.anh = lp.ANH;
+                var giaPhongs = lp.PHONGs.Where(p => p.MA_TRANGTHAI != "HU").Select(p => (int)p.GIAPHONG);
+                loaiPhongGioiThieu.giaMin = giaPhongs.Min();
+                loaiPhongGioiThieu.giaMax = giaPhongs.Max();
+                return loaiPhongGioiThieu;
+            });
+            return View(loaiPhongs.ToList());
+        }
+    }
+}
